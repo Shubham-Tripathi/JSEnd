dd701cda959b552fad7c742f8d5eeb0adfd5f2f6
ATMECS\Shubham.Tripathi <shubham.tripathi@atmecs.com>
2016-12-22 01:47:25 +0530

    Addedin initial setup files

---
+++ config/lib/mongoose.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose');
+
+// Initialize Mongoose
+module.exports.connect = function (cb) {
+  var _this = this;
+
+  var db = mongoose.connect('', function (err) {
+    // Log Error
+    if (err) {
+      console.log('Could not connect to MongoDB!');
+      console.log(err);
+    } else {
+
+      // Call callback FN
+      if (cb) cb(db);
+    }
+  });
+};
